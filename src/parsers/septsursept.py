b0fc692bfb347f399d5ca5a80bb215cf4507c329
sevas <f.degroef@gmail.com>
2011-04-04 22:35:14 +0200

    [7sur7] started

---
+++ src/parsers/septsursept.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import locale
+import urllib
+from datetime import datetime
+from itertools import chain
+import re
+from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup, UnicodeDammit, Tag
+from utils import fetch_html_content, count_words, make_soup_from_html_content
+
+
+
+
+
+
+
+
+
+
+
+
+def show_frontpage_articles():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    show_frontpage_articles()
