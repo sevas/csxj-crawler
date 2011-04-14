d1a54ed4b0f35d873a0a142023bfe24fd3ec1c5b
sevas <f.degroef@gmail.com>
2011-04-14 17:08:11 +0200

    [Le Soir] reorganized the ArticleData class, made a distinction between publication date extracted from the html and download date.

--- src/parsers/lesoir.py
+++ src/parsers/lesoir.py
@@ -31,32 +31,76 @@ def tag_URL((url, title), tags):
 
     
 class ArticleData(object):
-    def __init__(self, url, title, date, content, external_links, internal_links, category, author, intro):
+    """
+    A glorified dict to keep the extracted metadata and content of one article.
+    Has utility methods for json (de)serialization.
+    """
+    
+    def __init__(self, url, title,
+                 pub_date, pub_time, fetched_datetime,
+                 external_links, internal_links,
+                 category, author,
+                 intro, content):
+        """
+        Boring init func.
+        """
         self.url = url
         self.title = title
-        self.date = date
-        self.content = content
-        self.internal_links = internal_links
+        self.pub_date = pub_date
+        self.pub_time = pub_time
+        self.fetched_datetime = fetched_datetime
+
         self.external_links = external_links
+        self.internal_links = internal_links
+        
         self.category = category
         self.author = author
-        self.intro = intro
-
-
-    def to_json(self):
-        d = dict(self.__dict__)
         
-        date = d['date']
-        d['date'] = date.strftime('%Y-%m-%dT%H:%M:%S')
+        self.intro = intro
+        self.content = content
+
+
+        
+    def to_json(self):
+        """
+        Converts all attributes in self.__dict__ into a json string.
+        Takes care of non natively serializable objects (such as datetime).
+        """
+        d = dict(self.__dict__)
+        # datetime, date and time objects are not json-serializable
+
+        date = d['fetched_datetime']
+        d['fetched_datetime'] = date.strftime('%Y-%m-%dT%H:%M:%S')
+
+        pub_date, pub_time = d['pub_date'], d['pub_time']
+
+        d['pub_date'] = pub_date.strftime("%Y-%m-%d")
+        d['pub_time'] = pub_time.strftime("%H:%S")
+        
         return json.dumps(d)
 
 
+    
     @classmethod
     def from_json(kls, json_string):
+        """
+        Class method to rebuild an ArticleData object from a json string.
+        Takes care of non natively deserializable objects (such as datetime).        
+        """
         d = json.loads(json_string)
-        date_string = d['date']
-        d['date'] = datetime.strptime('%Y-%m-%dT%H:%M:%S')
+
+        date_string = d['fetched_datetime']
+        d['fetched_datetime'] = datetime.strptime('%Y-%m-%dT%H:%M:%S')
+
+        pub_date, pub_time = d['pub_date'],  d['pub_time']
+        year, month, day = [int(i) for i in pub_date.split('-')]
+        d['pub_date'] = date(year, month, day)
+
+        h, m = [int(i) for i in pub_time.split(':')]
+        d['pub_time'] = time(h, m)
+        
         return kls(**d)
+
 
 
 
@@ -65,6 +109,8 @@ TEXT_MARKUP_TAGS = ['b', 'i', 'u', 'em', 'tt', 'h1',  'h2',  'h3',  'h4',  'h5',
 
 def sanitize_fragment(fragment):
     """
+    Returns the plain text version of a chunk of text formatted with HTML tags.
+    Unsupported tags are ignored.
     """
     
     # A text fragment is either an HTML tag (with its own child text fragments)
@@ -83,7 +129,9 @@ def sanitize_fragment(fragment):
     
     
 def sanitize_paragraph(paragraph):
-    """removes image links, removes paragraphs, formatting"""
+    """
+    Removes image links, removes paragraphs, formatting
+    """
     return "".join([sanitize_fragment(fragment) for fragment in paragraph.contents])
 
 
@@ -176,7 +224,9 @@ def extract_date(story):
     date = header.find("p", {'class':"info st_date"})
 
     date_string = date.contents[0]
-    return datetime.strptime(date_string, "%A %d %B %Y, %H:%M")
+    datetime_published = datetime.strptime(date_string, "%A %d %B %Y, %H:%M")
+
+    return datetime_published.date(), datetime_published.time()
     
 
 
@@ -197,24 +247,30 @@ def extract_category(story):
     return category_stages
 
 
-def extract_article_data_from_html_content(html_content):
+def extract_article_data_from_url(url):
+    html_content = fetch_html_content(url)
     soup = make_soup_from_html_content(html_content)
     
     story = soup.find("div", {'id':'story'})
 
-    content = extract_content(story)
+
     category = extract_category(story)
-    title = extract_title(story)
-    
-    date = extract_date(story)    
+    title = extract_title(story)    
+    pub_date, pub_time = extract_date(story)
+    fetched_datetime = datetime.today()
     author = extract_author_name(story)
-    intro = extract_intro(story)
-    
     external_links, internal_links = extract_links(soup)
 
-    return title, content, category, date, external_links, internal_links, author, intro
+    intro = extract_intro(story)
+    content = extract_content(story)
 
+    fetched_datetime = datetime.today()
 
+    return ArticleData(url, title, pub_date, pub_time, fetched_datetime,
+                              external_links, internal_links,
+                              category, author,
+                              intro, content)
+    
 
 
 def get_two_columns_stories(element):
@@ -281,7 +337,6 @@ def get_frontpage_articles():
             else:
                 frontpage_links.append((item.h2.a.get("title"), item.h2.a.get("href")))
 
-
     return frontpage_links
 
 
@@ -300,21 +355,21 @@ def get_rss_articles():
     
 
     
-def parse_sample_data():
-    import sys
-    data_directory = "../../sample_data" 
-    sys.path.append(data_directory)
-    from dataset import dataset
-
-
-    for entry in dataset['le soir']:
-        url = entry['URL']
-        filename = entry['file']
-        filepath = "%s/%s" % (data_directory, filename)
-    
-        with open(filepath) as f:
-            html_content = f.read()
-            extract_article_data_from_html_content(html_content)
+#def parse_sample_data():
+#    import sys
+#    data_directory = "../../sample_data" 
+#    sys.path.append(data_directory)
+#    from dataset import dataset
+# 
+# 
+#    for entry in dataset['le soir']:
+#        url = entry['URL']
+#        filename = entry['file']
+#        filepath = "%s/%s" % (data_directory, filename)
+#    
+#        with open(filepath) as f:
+#            html_content = f.read()
+#            extract_article_data_from_html_content(html_content)
     
 
 
@@ -335,24 +390,31 @@ def separate_articles_from_blogposts(frontpage_links):
 
 
 
+
+ErrorLogEntry = namedtuple('ErrorLogEntry', 'url filename stacktrace')
+
+
 def get_frontpage_articles_data():
+    import traceback
     frontpage_links = get_frontpage_articles()
     article_links, blogpost_links = separate_articles_from_blogposts(frontpage_links)
 
     articles = []
-    
+    errors = []
+
     for (title, url) in article_links:
-        full_url = "http://www.lesoir.be%s" % url
-
-        html_content = fetch_html_content(full_url)
-        extracted_data = extract_article_data_from_html_content(html_content)
-        
-        title, content, category, date, external_links, internal_links, author, intro = extracted_data
-        article_data = ArticleData(full_url, title, date, content, external_links, internal_links, category, author, intro)
-
-        articles.append(article_data)
-
-    return articles, blogpost_links
+        try:
+            full_url = "http://www.lesoir.be%s" % url
+            
+            article_data = extract_article_data_from_url(full_url)
+            articles.append(article_data)
+        except AttributeError:
+            
+            stacktrace = traceback.format_stack()
+            new_error = ErrorLogEntry(full_url, None, stacktrace)
+            errors.append(new_error)
+     
+    return articles, blogpost_links, errors
 
 
     
@@ -372,14 +434,14 @@ def save_to_json_file(json_entries, outfilename, outdir):
 
 
 if __name__ == '__main__':
-    articles, blogpost_links = get_frontpage_articles_data()
+    articles, blogpost_links, errors = get_frontpage_articles_data()
 
     json_entries = []
     
     for article_data in articles:
         print "title = ", article_data.title
         print "url = ",  article_data.url
-        print "date = ", article_data.date
+        print "date = ", article_data.pub_date
         print "n external links = ", len(article_data.external_links)
         print "n internal links = ", len(article_data.internal_links)
         print "category = ", "/".join(article_data.category)
@@ -392,14 +454,14 @@ if __name__ == '__main__':
         json_entries.append(article_data.to_json())
         
         print "-" * 80
-
-    
         
     print "blogposts : "
-    print "\n".join(["%s (%s)" % (title, url) for (title, url) in blogpost_links])
 
-    result = {'articles':json_entries, 'blogposts':blogpost_links}
 
+    result = {'articles':json_entries, 'blogposts':blogpost_links, 'errors':errors}
+
+
+    print errors
     filename = make_json_filename('lesoir')
     save_to_json_file(result, filename, "../../out")
 
