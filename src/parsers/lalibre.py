1a8711d1920a3bae694d80593075c472cbaec714
sevas <f.degroef@gmail.com>
2011-04-21 17:34:15 +0200

    [La Libre] extracting publication time when available, fixed the link extraction, tags the links according to icon type in the link list, check if the intro section is actually there, added more samples

--- src/parsers/lalibre.py
+++ src/parsers/lalibre.py
@@ -27,14 +27,13 @@ def extract_date(main_content):
         date_string = fragments[4]
         print fragments
         h, m = [int(i) for i in fragments[-1].split(':')]
-        pub_date = time(h, m)
+        pub_time = time(h, m)
     else:
         date_string = publication_date.replace('Mis en ligne le ', '')
-        pub_date = None
+        pub_time = None
 
-    print date_string, pub_date
-    date = datetime.strptime(date_string, '%d/%m/%Y')
-    return date
+    pub_date = datetime.strptime(date_string, '%d/%m/%Y')
+    return pub_date.date(), pub_time
 
 
 
@@ -89,18 +88,54 @@ def extract_category(main_content):
     return [link.contents[0].rstrip().lstrip() for link in links]
 
 
-def extract_links(main_content):
+
+icon_type_to_tags = {
+    'pictoType0':['internal', 'full url'],
+    'pictoType1':['internal' 'local url'],
+    'pictoType2':['images'],
+    'pictoType3':['video'],
+    'pictoType4':['animation'],
+    'pictoType5':['audio'],
+    'pictoType9':['internal blog'],
+    'pictoType12':['external']
+}
+
+
+
+def make_tagged_url(title, url, icon_type):
+    """
+    Attempts to tag a url using the icon used. Mapping is incomplete at the moment.
+    Still keeps the icon type as part of the tags for future uses.
+    """
+    tags = [icon_type]
+    if icon_type in icon_type_to_tags:
+        tags.extend(icon_type_to_tags[icon_type])
+
+    return tag_URL((url, title), tags)
+
+
+
+def extract_and_tag_links(main_content):
+    """
+    Extract the associated links. Uses the icon type to tag it.
+    
+    """
     link_list = main_content.find('ul', {'class':'articleLinks'})
 
     # sometimes there are no links, and thus no placeholder
     if link_list:
         links = []
-        for entry in link_list.findAll('li', {'class':'picoType1'}, recursive=False):
-            title = entry.a.get('href')
-            url = entry.a.contents[0]
-            links.append((title, url))
+        for item in link_list.findAll('li', recursive=False):
+            # sometimes list items are used to show things which aren't links
+            # but more like unclickable ads
+            if item.a:
+                title = item.a.get('href')
+                url = item.a.contents[0]
+                icon_type = item.get('class')
+            
+                links.append(make_tagged_url(title, url, icon_type))
 
-        return ['http://www.lalibre.be%s' % link for link in links]
+        return links
     else:
         return []
         
@@ -113,8 +148,16 @@ def extract_author_name(main_content):
     else:
         return 'None'
 
-    
 
+def extract_intro(main_content):
+    hat = main_content.find('div', {'id':'articleHat'})
+
+    if hat:
+        return hat.contents[0].rstrip().lstrip()
+    else:
+        return ''
+
+    
 def extract_article_data(source):
     """
     """
@@ -130,20 +173,20 @@ def extract_article_data(source):
     category = extract_category(main_content)
     author = extract_author_name(main_content)
     title = main_content.h1.contents[0].rstrip().lstrip()
-    pub_date = extract_date(main_content)
+    pub_date, pub_time = extract_date(main_content)
     
-    intro = main_content.find('div', {'id':'articleHat'}).contents[0].rstrip().lstrip()
+    intro = extract_intro(main_content)
 
     content, keyword_links = extract_text_content_and_links(main_content)
 
-    article_links = extract_links(main_content)
+    tagged_article_links = extract_and_tag_links(main_content)
 
-    external_links = [tag_URL(i, ['to read']) for i in article_links]
+    external_links = tagged_article_links
     internal_links = [tag_URL(i, ['keyword']) for i in keyword_links]
 
     fetched_datetime = datetime.today()
 
-    new_article = ArticleData(source, title, pub_date, None, fetched_datetime,
+    new_article = ArticleData(source, title, pub_date, pub_time, fetched_datetime,
                               external_links, internal_links, category, author,
                               intro, content)
     return new_article
@@ -168,29 +211,15 @@ def get_frontpage_toc():
 
 
 
-#def test_sample_data():
-#    with open('../../sample_data/la_libre_recursive_cleanup.html', 'r') as f:
-#        html_content = f.read()
-#
-#        extracted_data = extract_article_data_from_html_content(html_content)
-#        category, author, title, date, intro, content, links = extracted_data
-#        url = 'foo'
-#        article_data = ArticleData(url, title, date, content, links, category, author, intro)
-#
-#        print 'title = ', article_data.title
-#        print 'url = http://www.lalibre.be%s' % article_data.url
-#        print 'date = ', article_data.date
-#        print 'n links = ', len(article_data.links)
-#        print 'category = ', '/'.join(article_data.category)
-#        print 'author = ', article_data.author
-#        print 'n words = ', count_words(article_data.content)
-#        print article_data.intro
-#        print
-#        print article_data.content
-#        print article_data.links
-                
+def test_sample_data():
+    filename = '../../sample_data/lalibre_no_links2.html'
+    with open(filename, 'r') as f:
+        article_data = extract_article_data(f)
+        article_data.print_summary()
 
+        article_data.url =  filename
 
+        print article_data.to_json()
 
 def list_frontpage_articles():
     frontpage_items = get_frontpage_toc()
@@ -201,6 +230,10 @@ def list_frontpage_articles():
 
         article = extract_article_data(url)
         article.print_summary()
+
+        print article.internal_links
+        print
+        print article.external_links
 
 
 
