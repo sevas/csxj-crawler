5ac70475fe1395289cf7a4d3b32a8bdc642c2816
sevas <f.degroef@gmail.com>
2011-04-28 20:20:40 +0200

    new test

--- src/test_parsers/test_articledata.py
+++ src/test_parsers/test_articledata.py
@@ -6,7 +6,10 @@ sys.path.append('../')
 
 import unittest
 import json
-from parsers.article import classify_and_tag, tag_URL, ArticleData
+from datetime import datetime, date, time
+
+from parsers.article import classify_and_tag, tag_URL, ArticleData, TaggedURL
+
 
 class URLClassificationTestCases(unittest.TestCase):
     def setUp(self):
@@ -48,10 +51,46 @@ class URLClassificationTestCases(unittest.TestCase):
         tags = classify_and_tag(url, self.own_netloc, self.associated_sites)
         self.assertEqual(tags, ['community forum'])
 
-        
-class JSONSerializationTestCases(unittest.TestCase):
-    pass
+
+
+    def test_tag_URL(self):
+        url, title = ('/news/category/2438985/', 'There is a hole in the world')
+        tags = ['internal']
+        taggedURL = TaggedURL(URL=url, title=title, tags=tags)
+        self.assertEqual(tag_URL((url, title), tags), taggedURL)
+
 
         
+class JSONSerializationTestCases(unittest.TestCase):
+    def setUp(self):
+        self.url = 'http://www.foo.com/news/category'
+        self.title = 'Fuck yeah royal wedding god damnit'
+        self.pub_date = date(2011, 04, 28)
+        self.pub_time = time(12, 23)
+        self.fetched_datetime = datetime(2011, 04, 28, 13, 01)
+
+        self.external_links = [TaggedURL(URL='http://www.bar.org/story.php?id=876543', title='I, Anteater', tags=['external'])]
+        self.internal_links = [TaggedURL(URL='/news/cat/754365', title='Platypus considered harmful', tags=['internal']),
+                               TaggedURL(URL='/define/platypus', title='Definition: Platypus', tags=['internal', 'keyword'])]
+
+        self.category = ['main', 'branch']
+        self.author = 'Some Person'
+
+        self.intro = 'So, that thing happenned'
+        self.content = ['Paragraphs', 'are', 'proof', 'of', 'your', 'literacy']
+
+#    def test_serialization(self):
+#        article = ArticleData(self.url, self.title,
+#                              self.pub_date, self.pub_time, self.fetched_datetime,
+#                              self.external_links, self.internal_links,
+#                              self.author, self.category,
+#                              self.intro, self.content)
+#
+#        json_string = article.to_json()
+#        article2 = ArticleData.from_json(json_string)
+#        self.assertEqual(article, article2)
+
+
+
 if __name__ == '__main__':
     unittest.main()
