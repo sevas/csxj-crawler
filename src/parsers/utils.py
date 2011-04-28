9a259d95bbae975d10acbbf9c0504c8ccce692f7
sevas <f.degroef@gmail.com>
2011-04-28 16:37:47 +0200

    added a way to detect urls in plaintext + associated testcases

--- src/parsers/utils.py
+++ src/parsers/utils.py
@@ -1,5 +1,7 @@
 import urllib
 from BeautifulSoup import BeautifulSoup
+import re
+
 
 def fetch_content_from_url(url):
     response = urllib.urlopen(url)
@@ -18,9 +20,24 @@ def fetch_rss_content(url):
     return fetch_content_from_url(url)
 
 
-
 def make_soup_from_html_content(html_content, convert_entities=True):
     if convert_entities:
         return BeautifulSoup(html_content, convertEntities=BeautifulSoup.HTML_ENTITIES)
     else:
         return BeautifulSoup(html_content)
+
+
+URL_MATCHER = re.compile('http://.*')
+
+def strip_punctuation(text):
+    punctuation_symbols = ';:,.(){}[]\'\"|\\/!?'
+    return text.lstrip(punctuation_symbols).rstrip(punctuation_symbols)
+
+def extract_plaintext_urls_from_text(some_text):
+    """
+    """
+    text_fragments = some_text.split(' ')
+    text_fragments = [strip_punctuation(f) for f in text_fragments]
+    urls = [f for f in text_fragments if URL_MATCHER.match(f)]
+    return urls
+    
---
+++ src/test_parsers/test_utils.py
@@ -0,0 +1,37 @@
+__author__ = 'sevas'
+
+import sys
+sys.path.append('../')
+import unittest
+
+from parsers.utils import extract_plaintext_urls_from_text
+
+class PlainTextURLExtractorTestCases(unittest.TestCase):
+    def setUp(self):
+        self.simple_url = 'http://www.foo.com'
+        # fuck yeah gehol
+        self.complex_url = 'http://164.15.72.157:8080/Reporting/Individual;Student%20Set%20Groups;id;%23SPLUS35F0F1?&template=Ann%E9e%20d%27%E9tude&weeks=1-14&days=1-6&periods=5-33&width=0&height=0'
+        self.text = 'This text was written in notepad, hence {0} , fuck you if you like clicking stuff'
+
+        
+    def test_simple_url(self):
+        text_with_url = self.text.format(self.simple_url)
+        urls = extract_plaintext_urls_from_text(text_with_url)
+        self.assertEqual(urls, [self.simple_url])
+
+
+    def test_complex_url(self):
+        text_with_url = self.text.format(self.complex_url)
+        urls = extract_plaintext_urls_from_text(text_with_url)
+        self.assertEqual(urls, [self.complex_url])
+
+
+    def test_multiple_urls(self):
+        text = 'this {0} has {1} many {2} links {3}'
+        text_with_urls = text.format(self.simple_url, self.complex_url, self.complex_url, self.simple_url)
+        urls = extract_plaintext_urls_from_text(text_with_urls)
+        self.assertEqual(urls, [self.simple_url, self.complex_url, self.complex_url, self.simple_url])
+
+    
+if __name__ == '__main__':
+    unittest.main()
