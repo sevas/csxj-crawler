1039552bec2d3367eaf9ad094eefda053597a1d5
sevas <f.degroef@gmail.com>
2011-05-17 17:06:19 +0200

    [RTLInfo] extract headlines in "modules". Separate blogposts and actual news tems

--- src/parsers/rtlinfo.py
+++ src/parsers/rtlinfo.py
@@ -1,10 +1,4 @@
-import sys
-from BeautifulSoup import BeautifulSoup, Tag
 from utils import fetch_content_from_url, make_soup_from_html_content, remove_text_formatting_markup
-
-
-
-
 
 
 
@@ -15,18 +9,35 @@ def extract_frontpage_title_and_url(link):
 
 
 def extract_headlines_from_module(module_container):
-    body = module_container.find('div', {'class':'ModuleElementBody'})
+    """
     
-    headline_lists = body.findAll('div', {'class':'tot_widget-ul'})
-    print len(headline_lists)
+    """
+    body = module_container.find('div', {'class':'ModuleElementBody'})
 
+    if body:
+        headline_lists = body.findAll('div', {'class':'tot_widget-ul'})
+
+        titles_and_urls = list()
+        for l in headline_lists:
+            for tag_name in ('h3', 'h5', 'li'):
+                titles_and_urls.extend([extract_frontpage_title_and_url(h.a) for h in l.findAll(tag_name) if h.a])
+
+        return titles_and_urls
+    else:
+        return []
 
 
 def extract_headlines_from_modules(maincontent):
-    module_zone = maincontent.findAll('div', {'id':'hp-zone-list'})
+    module_zone = maincontent.find('div', {'id':'hp-zone-list'})
 
     all_modules = module_zone.findAll('div', {'class':'ModuleElement'})
 
+    headlines = list()
+    for module in all_modules:
+        module_headlines = extract_headlines_from_module(module)
+        headlines.extend(module_headlines)
+
+    return headlines
 
 
 def extract_small_articles(maincontent):
@@ -61,6 +72,15 @@ def make_full_url((title, url)):
         return title, 'http://www.rtl.be{0}'.format(url)
 
 
+def separate_news_and_blogposts(titles_and_urls):
+    all_items = set(titles_and_urls)
+    blogposts = set([(title, url) for title, url in all_items if not url.startswith('/')])
+    news_items = all_items - blogposts
+
+    return news_items, blogposts
+
+
+
 def get_frontpage_toc():
     url = 'http://www.rtl.be/info/'
     html_content = fetch_content_from_url(url)
@@ -70,13 +90,19 @@ def get_frontpage_toc():
 
     first_articles = extract_first_articles(maincontent)
     small_articles = extract_small_articles(maincontent)
+    modules_articles = extract_headlines_from_modules(maincontent)
 
-    all_articles = first_articles + small_articles
+    all_articles = first_articles + small_articles + modules_articles
 
-    return [make_full_url(title_and_url) for title_and_url in all_articles]
+    news_items, blogposts = separate_news_and_blogposts(all_articles)
+
+    return [make_full_url(title_and_url) for title_and_url in news_items], blogposts
 
 
 if __name__=='__main__':
-    toc = get_frontpage_toc()
+    toc, blogs = get_frontpage_toc()
+
+    print len(toc)
     for title, url in toc:
         print title, url
+
