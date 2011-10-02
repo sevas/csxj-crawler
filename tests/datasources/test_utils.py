b68ad1cd04bfd7ff48b149f9e8b59939c2feccc0
sevas <f.degroef@gmail.com>
2011-10-02 21:19:28 +0200

    unit tests for the html cleanup utility funcs

--- tests/datasources/test_utils.py
+++ tests/datasources/test_utils.py
@@ -2,8 +2,10 @@ __author__ = 'sevas'
 
 import sys
 import unittest
+from BeautifulSoup import BeautifulSoup
 
 from csxj.datasources.common.utils import extract_plaintext_urls_from_text
+from csxj.datasources.common.utils import remove_text_formatting_markup_from_fragments
 
 class PlainTextURLExtractorTestCases(unittest.TestCase):
     def setUp(self):
@@ -46,6 +48,58 @@ class PlainTextURLExtractorTestCases(unittest.TestCase):
         urls = extract_plaintext_urls_from_text(text)
         self.assertEqual(urls, [])
         
+
+
+def make_fragments(html_data):
+    soup = BeautifulSoup(html_data)
+    return soup.contents
+
+class HTMLCleanupTestCases(unittest.TestCase):
+    def test_one_tag(self):
+        html_fragments = make_fragments(u"<strong>hello</strong>")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"hello")
+
+
+    def test_multiple_tags(self):
+        html_fragments = make_fragments(u"<em><strong>hello</strong><em>")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"hello")
+
+
+    def test_unsupported_tag(self):
+        html_fragments = make_fragments(u"<foo>hello</foo>")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"")
+
+
+    def test_link(self):
+        html_fragments = make_fragments(u"""<a href="/foo">foo</a>""")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"foo")
+
+
+    def test_multiple_fragments(self):
+        html_fragments = make_fragments(u"<strong>foo</strong><em>bar</em>")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"foobar")
+
+
+    def test_mixed_carriage_returns(self):
+        html_fragments = make_fragments(u"<i>foo</i>\n<b>bar</b>")
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"foo\nbar")
+
+
+    def test_cascading_formatting(self):
+        html_fragments = make_fragments("""
+        <p><strong>First</strong> paragraph.</p>
+        <p><strong>Second</strong> paragraph.</p>
+        """)
+        clean_fragments = remove_text_formatting_markup_from_fragments(html_fragments)
+        self.assertEquals(clean_fragments, u"\nFirst paragraph.\nSecond paragraph.\n")
+
+
 
 if __name__ == '__main__':
     unittest.main()
