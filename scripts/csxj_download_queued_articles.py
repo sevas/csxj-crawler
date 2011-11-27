013d7e21fc05cfd1e408c5888f51586b4d3a3700
sevas <f.degroef@gmail.com>
2011-11-27 21:47:52 +0100

    don't stop the entire process if one source threw an exception

--- scripts/csxj_download_queued_articles.py
+++ scripts/csxj_download_queued_articles.py
@@ -9,7 +9,6 @@ from csxj.articlequeue import ArticleQueueDownloader
 
 
 
-
 def download_all_queued_articles(db_root):
     if not os.path.exists(db_root):
         print("no such database directory: {0}".format(db_root))
@@ -17,13 +16,14 @@ def download_all_queued_articles(db_root):
         ArticleQueueDownloader.setup_logging()
         all_sources = lesoir, dhnet, lalibre, sudpresse, rtlinfo
         for source in all_sources:
-            csxj.crawler.download_queued_articles(source, db_root)
-
+            try:
+                csxj.crawler.download_queued_articles(source, db_root)
+            except Exception as e:
+                print e.message
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download, analyze and store all the queued articles in the csxj json database')
-    parser.add_argument('--debug', dest='debug', action='store_true', help="run crawler in debug mode")
     parser.add_argument('--outdir', type=str, dest='outdir', required=True, help='directory to dump the json db in')
     args = parser.parse_args()
 
