bf187c46c2d608b63198520fa46d269b9bca441f
sevas <f.degroef@gmail.com>
2011-11-27 21:24:10 +0100

    added setup.py for package installation

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
+from setuptools import setup
+import sys
+import csxj
 
-  
+setup(name='csxj',
+      package_dir={'': '.'},
+      packages=['csxj', 'csxj.datasources', 'csxj.db'],
+      version=csxj.__version__,
+
+      #PyPI metadata
+      url=['http://bitbucket.org/sevas/csxj-crawler'],
+      keywords=[''],
+      platforms='All',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Utilities"])
