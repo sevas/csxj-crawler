c39764f5feb2cb26633b087c4f2f37e9234a65da
sevas <f.degroef@gmail.com>
2012-01-16 00:21:56 +0100

    more info on queued items when listing

--- scripts/csxj_queue_troubleshooting.py
+++ scripts/csxj_queue_troubleshooting.py
@@ -5,18 +5,26 @@ from csxj.datasources import rtlinfo, sudpresse, lesoir, lalibre, dhnet
 
 def show_queue_info(json_db):
     sources = [rtlinfo, sudpresse, lesoir, lalibre, dhnet]
+    res = dict()
     for source in sources:
         p = Provider(json_db, source.SOURCE_NAME)
-
+        print source.SOURCE_NAME
         batches_by_day = p.get_queued_batches_by_day()
+        total_item_count = 0
 
         for day, batches in batches_by_day:
+            print "\tDay:", day
             queued_item_count = 0
             for batch, items in batches:
-
                 queued_item_count += len(items['articles'])
-            print day, queued_item_count
 
+            print day, queued_item_count
+            total_item_count += queued_item_count
+        res[source.SOURCE_NAME] = (total_item_count, len(batches_by_day))
+
+
+    for name, (item_count, day_count) in res.items():
+        print "{0}: {1} items for {2} days".format(name, item_count, day_count)
 
 def try_download_queue(json_db):
     sources = [rtlinfo, sudpresse, lesoir, lalibre, dhnet]
@@ -40,14 +48,13 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Utility functions to troubleshoot queue management')
     parser.add_argument('--jsondb', type=str, dest='jsondb', required=True, help='json db root directory')
-    #('--show-only', type=bool, dest='show_queue', )
 
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--download-queue',  action='store_true')
     group.add_argument('--show-queue', action='store_true')
 
     args = parser.parse_args()
-    print args
+
     if args.download_queue:
         try_download_queue(args.jsondb)
     elif args.show_queue:
