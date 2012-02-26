e1cd60c1671441b25206b93f520b6e06957145ee
sevas <f.degroef@gmail.com>
2012-02-26 23:50:55 +0100

    started sudinfo revamping using scrapy

--- csxj/datasources/sudinfo.py
+++ csxj/datasources/sudinfo.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from datetime import datetime, date, time
+from datetime import datetime
 import locale
 from itertools import chain
 import urllib
+import codecs
+
 from BeautifulSoup import Tag
 from scrapy.selector import HtmlXPathSelector
+
 from common.utils import make_soup_from_html_content, fetch_content_from_url, fetch_html_content
 from common.utils import extract_plaintext_urls_from_text
 from common.utils import remove_text_formatting_markup_from_fragments
-from csxj.common.tagging import tag_URL, classify_and_tag, make_tagged_url, TaggedURL
+from csxj.common.tagging import  classify_and_tag, make_tagged_url
 from csxj.db.article import ArticleData
 
 
@@ -19,10 +22,14 @@ if sys.platform in ['linux2', 'cygwin']:
     locale.setlocale(locale.LC_TIME, 'fr_FR.UTF8')
 elif sys.platform in [ 'darwin']:
     locale.setlocale(locale.LC_TIME, 'fr_FR')
+elif sys.platform in [ 'win32']:
+    # locale string from: http://msdn.microsoft.com/en-us/library/cdax410z(v=VS.80).aspx
+    locale.setlocale(locale.LC_ALL, 'fra')
 
 
 SUDINFO_INTERNAL_SITES = {
-    'portfolio.sudinfo.be':['internal site', 'images', 'gallery']
+    'portfolio.sudinfo.be':['internal site', 'images', 'gallery'],
+    'portfolio.sudpresse.be':['internal site', 'images', 'gallery']
 }
 
 SUDINFO_OWN_NETLOC = 'sudinfo.be'
@@ -30,41 +37,17 @@ SUDINFO_OWN_NETLOC = 'sudinfo.be'
 SOURCE_TITLE = u"Sud Info"
 SOURCE_NAME = u"sudinfo"
 
-def extract_category(content):
-    breadcrumbs = content.find('p', {'class':'ariane'})
-    if breadcrumbs:
-        return [link.contents[0].strip() for link in breadcrumbs.findAll('a')]
-    else:
-        alternate_breadcrumbs = content.find('p', {'class':'fil_ariane left'})
-        return [link.contents[0].strip() for link in alternate_breadcrumbs.findAll('a')]
 
-
-def extract_title(article):
-    return article.h1.contents[0].strip()
-
-
-
-def extract_date(article):
-    pub_date_container = article.find('p', {'class':'publiele'})
-
+def extract_date(hxs):
     today = datetime.today()
 
-    date_string = pub_date_container.span.contents[0]
-    d, m = [int(i) for i in date_string.split('/')]
-    pub_date = date(today.year, m, d)
+    date_string = u"".join(hxs.select("//p[@class='publiele']//text()").extract())
+    date_bytestring = codecs.encode(date_string, 'utf-8')
+    date_fmt = codecs.encode(u"Publié le %A %d %B %Y à %Hh%M", 'utf-8')
 
-    hour, minutes = pub_date_container.contents[2], pub_date_container.contents[4]
-    h = int(hour.split(u'à')[1])
-    m = int(minutes)
-    pub_time = time(h, m)
+    datetime_published = datetime.strptime(date_bytestring, date_fmt)
 
-    return pub_date, pub_time
-
-
-
-def extract_author_name(article):
-    return article.find('p', {'class':'auteur'}).contents[0].strip()
-
+    return datetime_published.date(), datetime_published.time()
 
 
 def extract_text_and_links_from_paragraph(paragraph):
@@ -103,8 +86,8 @@ def extract_text_and_links_from_paragraph(paragraph):
 
 
 
-def extract_intro_and_links(article):
-    intro_container = article.find('p', {'class':'chapeau'})
+def extract_intro_and_links(hxs):
+    intro_container = hxs.select("//p[@class='chapeau']/text()")
     intro_text, tagged_urls = extract_text_and_links_from_paragraph(intro_container)
     return intro_text, tagged_urls
 
@@ -132,75 +115,77 @@ LINK_TYPE_TO_TAG = {
 }
 
 
-def extract_associated_links(article):
-    links_block = article.find('div', {'class':'bloc-01'})
+def extract_associated_links(hxs, source_url):
+    links = hxs.select("//div[@id='picture']/descendant::div[@class='bloc-01']//a")
 
-    if links_block:
-        link_list = links_block.find('ul')
+    all_tagged_urls = []
 
-
-        def extract_url_and_title(item):
-            url = item.a.get('href')
-            title = remove_text_formatting_markup_from_fragments(item.a.contents)
+    if links:
+        def extract_url_and_title(link_hxs):
+            url = link_hxs.select('@href').extract()[0]
+            title = u"".join(link_hxs.select("text()").extract())
 
             tags = set()
             if not title:
                 title = u'No Title'
                 tags.add('ghost link')
+            if not url:
+                url = u''
+                tags.add('no target')
             return url, title, tags
 
         all_tagged_urls = list()
-        for item in link_list.findAll('li'):
+        for item in links:
             url, title, tags = extract_url_and_title(item)
             tags.update(classify_and_tag(url, SUDINFO_OWN_NETLOC, SUDINFO_INTERNAL_SITES))
 
-            link_type = item.get('class')
-            if  link_type in LINK_TYPE_TO_TAG:
+            link_type = item.select('@class')
+            if link_type and link_type[0] in LINK_TYPE_TO_TAG:
                 tags.update(LINK_TYPE_TO_TAG[link_type])
 
             all_tagged_urls.append(make_tagged_url(url, title, tags))
 
-        return all_tagged_urls
-    else:
-        return []
+
+    media_links = hxs.select("//div[@id='picture']/descendant::div[@class='bloc-01 pf_article']//a")
+
+    if media_links:
+        for i, item in enumerate(media_links):
+            url = "{0}{1}".format(source_url, item)
+            title = "EMBEDDED MEDIA {0}".format(i)
+            tags = set(['media', 'embedded'])
 
 
-def is_page_error_404(soup):
+    print all_tagged_urls
 
-    return soup.head.title.contents[0] == '404'
+    return all_tagged_urls
+
+
+def is_page_error_404(hxs):
+    return hxs.select("//head/title/text()").extract() == '404'
 
 
 
-def extract_article_data(source):
+def extract_article_data(source_url):
     """
     """
-    if hasattr(source, 'read'):
-        html_content = source.read()
-    else:
-        html_content = fetch_html_content(source)
+    html_content = fetch_html_content(source_url)
+    hxs = HtmlXPathSelector(text=html_content)
 
-    soup = make_soup_from_html_content(html_content)
-
-
-    if is_page_error_404(soup):
+    if is_page_error_404(hxs):
         return None, html_content
     else:
-        content = soup.find('div', {'id':'content'})
-        category = extract_category(content)
-
-        article = soup.find('div', {'id':'article'})
-        title = extract_title(article)
-        pub_date, pub_time = extract_date(article)
-        author = extract_author_name(article)
-
+        category = hxs.select("//p[starts-with(@class, 'fil_ariane')]/a//text()").extract()
+        title = hxs.select("//div[@id='article']/h1/text()").extract()[0]
+        pub_date, pub_time = extract_date(hxs)
+        author = hxs.select("//p[@class='auteur']/text()").extract()[0]
         fetched_datetime = datetime.today()
 
-        intro, intro_links = extract_intro_and_links(article)
-        content, content_links = extract_content_and_links(article)
+#        intro, intro_links = extract_intro_and_links(hxs)
+#        content, content_links = extract_content_and_links(article)
 
-        associated_links = extract_associated_links(article)
+        associated_links = extract_associated_links(hxs, source_url)
 
-        return ArticleData(source, title, pub_date, pub_time, fetched_datetime,
+        return ArticleData(source_url, title, pub_date, pub_time, fetched_datetime,
                            intro_links + content_links + associated_links,
                            category, author,
                            intro, content), html_content
@@ -213,10 +198,14 @@ def extract_title_and_url(container):
         return link.contents[0].strip(), link.get('href')
 
 
+
+
+
+### frontpage stuff
+
 def extract_title_url_from_hxs_a(link):
     title = ''.join(link.select(".//text()").extract())
     return title.strip(), link.select("@href").extract()[0]
-
 
 
 def get_regional_toc():
@@ -281,11 +270,9 @@ def test_sample_data():
         print article_data.content
 
 
-def download_one_article():
-    url = 'http://www.sudpresse.be/regions/liege/2012-01-09/liege-un-mineur-d-age-et-un-majeur-apprehendes-pour-un-viol-collectif-930314.shtml'
-    url = 'http://sudpresse.be/actualite/dossiers/2012-01-02/le-stage-du-standard-a-la-manga-infos-photos-tweets-928836.shtml'
-    #url = 'http://sudpresse.be/%3C!--%20error:%20linked%20page%20doesn\'t%20exist:...%20--%3E'
-    url = "http://sudpresse.be/actualite/faits_divers/2012-01-10/un-enfant-de-4-ans-orphelin-sa-mere-a-saute-sur-les-voies-pour-recuperer-son-gsm-930520.shtml"
+def show_article():
+    url = "http://www.sudinfo.be/336280/article/sports/foot-belge/anderlecht/2012-02-26/lierse-anderlecht-les-mauves-vont-ils-profiter-de-la-defaite-des-brugeois"
+    url = "http://www.sudinfo.be/335985/article/sports/foot-belge/charleroi/2012-02-26/la-d2-en-direct-charleroi-gagne-en-l-absence-d-abbas-bayat-eupen-est-accro"
     article_data, raw_html = extract_article_data(url)
 
     if article_data:
@@ -294,6 +281,9 @@ def download_one_article():
         print article_data.content
     else:
         print 'no article found'
+
+
+
 
 
 def show_frontpage_toc():
@@ -305,5 +295,6 @@ def show_frontpage_toc():
 
 
 if __name__=='__main__':
-    show_frontpage_toc()
     #download_one_article()
+    #show_frontpage_articles()
+    show_article()
