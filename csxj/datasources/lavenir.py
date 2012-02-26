19e885dcfe5ace81a1b3a4ad5949f87bc5d8cfb5
sevas <f.degroef@gmail.com>
2012-02-26 21:33:37 +0100

    reorganized imports

--- csxj/datasources/lavenir.py
+++ csxj/datasources/lavenir.py
@@ -4,16 +4,17 @@
 import sys
 import locale
 from datetime import datetime
-from scrapy.selector import HtmlXPathSelector
-import urlparse
 import codecs
 from itertools import izip
+
+from scrapy.selector import HtmlXPathSelector
+
 from csxj.common.tagging import tag_URL, classify_and_tag, make_tagged_url, TaggedURL
 from csxj.db.article import ArticleData
-from common.utils import fetch_html_content, fetch_rss_content, make_soup_from_html_content
-from common.utils import remove_text_formatting_markup, extract_plaintext_urls_from_text
+from common.utils import fetch_html_content
+from common.utils import extract_plaintext_urls_from_text
 from common import constants
-from itertools import izip
+
 
 
 # for datetime conversions
