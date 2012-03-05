676b8f0812d12571f1df5807d4a1ac5f9aaf1cb0
sevas <f.degroef@gmail.com>
2012-03-05 02:15:10 +0100

    added frontpage scrapper for 7sur7

--- csxj/datasources/septsursept.py
+++ csxj/datasources/septsursept.py
@@ -1,64 +1,91 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys
-import locale
-import urllib
-from datetime import datetime
 from itertools import chain
-import re
-from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup, UnicodeDammit, Tag
-from common.utils import fetch_html_content, make_soup_from_html_content
+import urlparse
+
+from scrapy.selector import HtmlXPathSelector
+from csxj.common.tagging import tag_URL, classify_and_tag, make_tagged_url, TaggedURL
+from csxj.db.article import ArticleData
+from common.utils import fetch_html_content
+from common.utils import setup_locales
+
+
+setup_locales()
+
+
+SOURCE_TITLE = u"7 sur 7"
+SOURCE_NAME = u"septsursept"
+
+SEPTSURSEPT_INTERNAL_BLOGS = {}
+
+SEPTSURSEPT_NETLOC = "7sur7.be"
 
 
 
 
 
-
-def extract_titles_and_links_from_third_column(third_column):
-    """
-    """
-    items = third_column.findAll('div', {'class':'gen_imgbox'}, recursive=False)
-   
-    def extract_title_and_link(imgbox):
-        return imgbox.h3.a.contents, imgbox.h3.a.get('href')
-
-    return [extract_title_and_link(imgbox) for imgbox in items]
+def make_full_url(item):
+    title, url = item
+    if not url.startswith("http"):
+        return title, "http://{0}{1}".format(SEPTSURSEPT_NETLOC, url)
+    else:
+        return title, url
 
 
 
-def extract_titles_and_links_from_second_column(second_column):
-    return []
+def extract_title_and_url(link_selector):
+    url = link_selector.select("./@href").extract()[0]
+    title = link_selector.select("./text()").extract()[0].strip()
+    return title, url
+
+
+def get_frontpage_toc():
+    html_data = fetch_html_content('http://7sur7.be')
+    hxs = HtmlXPathSelector(text=html_data)
+
+    # get all the stories from the left column
+    first_story = hxs.select("//section//article/h1/a")
+    main_stories = hxs.select("//section//article/h3/a")
+
+    #this news site is terrible. really.
+    random_crap_stories = hxs.select("//section//section [starts-with(@class, 'tn_you')]//li/h3/a")
+    more_crap_stories = hxs.select("//section//section [starts-with(@class, 'tn_hln')]//li/h3/a")
+
+    all_left_stories = chain(first_story, main_stories, random_crap_stories, more_crap_stories)
+    left_items = [extract_title_and_url(link_hxs) for link_hxs in all_left_stories]
+
+    # get all the stories from the right column
+    all_side_stories = hxs.select("//section [@class='str_aside_cntr']//h3/a")
+    stories_to_remove = hxs.select("//section [@class='str_aside_cntr']//section [starts-with(@class, 'teas_article_306')]//li/h3/a")
+
+    right_items = set([extract_title_and_url(link_hxs) for link_hxs in all_side_stories])
+    to_remove = set([extract_title_and_url(link_hxs) for link_hxs in stories_to_remove])
+
+    right_items = list(right_items - to_remove)
 
 
 
-def extract_frontpage_titles_and_links():
-    
-    html_content = fetch_html_content('http://7sur7.be')
-
-    soup = make_soup_from_html_content(html_content, True)
-
-    second_column = soup.find('div', {'id':'str_col2'})
-    titles_and_links = extract_titles_and_links_from_second_column(second_column)
-    
-    third_column = soup.find('div', {'id':'str_col3'})
-    titles_and_links = extract_titles_and_links_from_third_column(third_column)
-
-    
-    
-    
-    
+    frontpage_items = left_items + right_items
+    return [make_full_url(item) for item in frontpage_items], []
 
 
-def show_frontpage_articles():
-    titles_and_links = extract_frontpage_titles_and_links()
-
-    for (title, link) in titles_and_links:
-        print '%s (%s)' % (title, link)
 
 
+def show_frontpage():
+    frontpage_items, blogposts = get_frontpage_toc()
+
+    print "NEWS ({0}):".format(len(frontpage_items))
+    for title, url in frontpage_items:
+        print u"{0} \t\t [{1}]".format(title, url)
+
+    print "\n\nBLOGPOSTS ({0}):".format(len(blogposts))
+    for title, url in blogposts:
+        print u"{0} \t\t [{1}]".format(title, url)
 
 
 
 
 if __name__ == '__main__':
-    show_frontpage_articles()
+    show_frontpage()
