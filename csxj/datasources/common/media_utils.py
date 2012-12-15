621daf61d834883e9440fba64ff695c805abbe80
sevas <f.degroef@gmail.com>
2012-12-15 18:17:44 +0100

    use source url for title when extracting info from embedded iframes

--- csxj/datasources/common/media_utils.py
+++ csxj/datasources/common/media_utils.py
@@ -1,7 +1,8 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
 def extract_url_from_iframe(iframe):
     url = iframe.get('src')
-    title = u"__EMBEDDED_CONTENT_IFRAME__"
+    title = url
     return url, title
