b092abdf5948276bcb3ad2aaea5b96bbd82ea0a3
sevas <f.degroef@gmail.com>
2013-01-15 00:02:52 +0100

    [csxj_test_tools] compare TaggedURL lists equality using sets before doing per-item comparison in sorted lists
    Because sometimes the exact same url can exist in several places, with different tags, and the and sorted list trick will not work

--- tests/datasources/csxj_test_tools.py
+++ tests/datasources/csxj_test_tools.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from nose.tools import eq_, nottest
+from nose.tools import eq_, ok_, nottest
+
+
+@nottest
+def to_frozensets(taggedURLs):
+    return set([(url, title, frozenset(tags)) for url, title, tags in taggedURLs])
 
 
 @nottest
@@ -12,7 +17,11 @@ def assert_taggedURLs_equals(expected_links, extracted_links):
     """
     expected_count, extracted_count = len(expected_links), len(extracted_links)
     eq_(expected_count, extracted_count, msg="Expected {0} links. Extracted {1}".format(expected_count, extracted_count))
-    for expected, extracted in zip(sorted(expected_links), sorted(extracted_links)):
-        eq_(expected[0], extracted[0], msg=u'URLs are not the same: \n\t{0} \n\t{1}'.format(expected[0], extracted[0]))
-        #eq_(expected[1], extracted[1], msg='titles are not the same')
-        eq_(expected[2], extracted[2], msg=u'[{0}]({1}): tags are not the same: \n\tExpected: {2} \n\tGot:      {3}'.format(expected[1], expected[0], expected[2], extracted[2]))
+
+    if to_frozensets(expected_links) != to_frozensets(extracted_links):
+        for expected, extracted in zip(sorted(expected_links), sorted(extracted_links)):
+            eq_(expected[0], extracted[0], msg=u'URLs are not the same: \n\t{0} \n\t{1}'.format(expected[0], extracted[0]))
+            #eq_(expected[1], extracted[1], msg='titles are not the same')
+            eq_(expected[2], extracted[2], msg=u'[{0}]({1}): tags are not the same: \n\tExpected: {2} \n\tGot:      {3}'.format(expected[1], expected[0], expected[2], extracted[2]))
+    else:
+        ok_(True)
