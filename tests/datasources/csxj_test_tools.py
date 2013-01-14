fd123d368fca4d6155aa0d7759008910a54a6779
sevas <f.degroef@gmail.com>
2013-01-14 22:30:24 +0100

    doctoring are nice

--- tests/datasources/csxj_test_tools.py
+++ tests/datasources/csxj_test_tools.py
@@ -4,7 +4,14 @@ from nose.tools import eq_, nottest
 
 @nottest
 def assert_taggedURLs_equals(expected_links, extracted_links):
-    eq_(len(expected_links), len(extracted_links), msg="Expected {0} links. Extracted {1}".format(len(expected_links), len(extracted_links)))
+    """ Helper function to assess the equality of two lists of TaggedURL
+
+        Provides somewhat helpful feedback to know what is actually different
+        It does not compare the 'title' fields because character encoding
+        issues make me cry. A lot.
+    """
+    expected_count, extracted_count = len(expected_links), len(extracted_links)
+    eq_(expected_count, extracted_count, msg="Expected {0} links. Extracted {1}".format(expected_count, extracted_count))
     for expected, extracted in zip(sorted(expected_links), sorted(extracted_links)):
         eq_(expected[0], extracted[0], msg=u'URLs are not the same: \n\t{0} \n\t{1}'.format(expected[0], extracted[0]))
         #eq_(expected[1], extracted[1], msg='titles are not the same')
