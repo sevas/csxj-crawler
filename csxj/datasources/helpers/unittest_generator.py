6c80a8d98b9a47e4143c21dd1724f92d2c986e16
sevas <f.degroef@gmail.com>
2013-02-03 18:16:00 +0100

    [tests] saving html for a specific generated test is optional

--- csxj/datasources/helpers/unittest_generator.py
+++ csxj/datasources/helpers/unittest_generator.py
@@ -15,7 +15,7 @@ def generate_test_func(fname, parser_name, urls_by_group):
 def save_sample_data_file(html_data, source_url, test_name, root_path):
     if os.path.exists(root_path):
 
-        full_path_name = os.path.join(root_path, test_name+".html")
+        full_path_name = os.path.join(root_path, test_name + ".html")
         if os.path.exists(full_path_name):
             print "{0} already exists. Delete it if you want to overwrite it. Aborting.".format(full_path_name)
             return
@@ -23,18 +23,18 @@ def save_sample_data_file(html_data, source_url, test_name, root_path):
         with open(full_path_name, 'w') as f:
             f.write(html_data)
 
-        full_index_name =os.path.join(root_path, "index.json")
+        full_index_name = os.path.join(root_path, "index.json")
         index = dict(test_data=[])
         if os.path.exists(full_index_name):
             with open(full_index_name, 'r') as f:
                 old_index = json.load(f)
                 index.update(old_index)
-            index['test_data'].append((source_url, test_name+".html"))
+            index['test_data'].append((source_url, test_name + ".html"))
             with open(full_index_name, 'w') as f:
                 json.dump(index, f, indent=2)
 
 
-def generate_unittest(test_name, parser_name, urls_by_group, html_data, source_url, test_data_root_path):
+def generate_unittest(test_name, parser_name, urls_by_group, html_data, source_url, test_data_root_path, save_file=False):
     """
     example usage, from within an extract_article_data() function:
         import os
@@ -44,10 +44,10 @@ def generate_unittest(test_name, parser_name, urls_by_group, html_data, source_u
                                                                     embedded_content_links=embedded_content_links,
                                                                     in_text_links=in_text_links),
                             html_content, source, os.path.join(os.path.dirname(__file__), "../../tests/datasources/test_data/", SOURCE_NAME))
-
     """
-    print ("-"*80)
-    print (generate_test_func(test_name, parser_name, urls_by_group))
-    print ("-"*80)
+    print("-" * 80)
+    print(generate_test_func(test_name, parser_name, urls_by_group))
+    print("-" * 80)
 
-    save_sample_data_file(html_data, source_url, test_name, test_data_root_path)
+    if save_file:
+        save_sample_data_file(html_data, source_url, test_name, test_data_root_path)
