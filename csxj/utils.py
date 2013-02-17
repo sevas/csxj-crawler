60bc8db8e741d8d1af379d0743d4435f0abb1ea9
sevas <f.degroef@gmail.com>
2013-02-17 18:29:58 +0100

    cosmetics

--- csxj/utils.py
+++ csxj/utils.py
@@ -2,20 +2,17 @@ import os.path
 import json
 
 
-
 def load_last_toc_from_file(filename):
     last_stories_fetched = list()
     if os.path.exists(filename):
         with open(filename, 'r') as f:
-            last_stories_fetched = [tuple(i) for i in  json.load(f)]
+            last_stories_fetched = [tuple(i) for i in json.load(f)]
     return last_stories_fetched
-
 
 
 def save_toc_to_file(toc, filename):
     with open(filename, 'w') as f:
         json.dump(toc, f)
-
 
 
 def filter_only_new_stories(frontpage_stories, filename):
