bba6eddd1e959ebc4a49ea7a87c420286fe79af3
sevas <f.degroef@gmail.com>
2013-03-03 20:15:00 +0100

    [reprocess] disabled multiprocessing is --processes=1

--- scripts/csxj_reprocess_entire_database.py
+++ scripts/csxj_reprocess_entire_database.py
@@ -1,11 +1,8 @@
-"""
+# coding=utf-8
 
-"""
 import os
-import os.path
 import json
 import traceback
-import multiprocessing as mp
 from datetime import datetime
 
 import csxj.db as csxjdb
@@ -24,6 +21,10 @@ NAME_TO_SOURCE_MODULE_MAPPING = {
     'septsursept': septsursept,
     'sudpresse': sudpresse
 }
+
+
+def make_parser_list():
+    return ','.join([p.SOURCE_NAME for p in NAME_TO_SOURCE_MODULE_MAPPING.values()])
 
 
 def write_dict_to_file(d, outdir, outfile):
@@ -49,30 +50,30 @@ def reprocess_single_batch(datasource_parser, raw_data_dir):
         for url, raw_file in index:
             raw_filepath = os.path.join(raw_data_dir, raw_file)
             try:
-                print u"    Reprocessing: {1} ({0})".format(url, raw_filepath)
-                #reprocessed_articles.append(raw_filepath)
+                print u"°°° Reprocessing: {1} ({0})".format(url, raw_filepath)
                 with open(raw_filepath, 'r') as raw_html:
                     article_data, html = datasource_parser.extract_article_data(raw_html)
                     article_data.url = url
                     reprocessed_articles.append((article_data, html))
             except Exception as e:
                 stacktrace = traceback.format_exc()
-                print u"!!! FAIL", e.message
+                print u"!!! FAIL", e
                 errors_encountered.append((url, raw_filepath, stacktrace))
 
     return reprocessed_articles, errors_encountered
 
 
 def save_reprocessed_batch(dest_root_dir, source_name, day_string, batch_hour_string, articles):
-    batch_root_dir = os.path.join(dest_root_dir, source_name, day_string, batch_hour_string)
-    if not os.path.exists(batch_root_dir):
-        os.makedirs(batch_root_dir)
 
+    batch_root_dir = os.path.join(dest_root_dir, source_name, day_string, batch_hour_string)
     print u"+++ Saving {0} articles to {1}".format(len(articles), batch_root_dir)
     articles_json_data = {"articles": [article.to_json() for article, raw_html in articles],
                           "errors": []}
-    articles_filepath = os.path.join(batch_root_dir, csxjdb.constants.ARTICLES_FILENAME)
-    write_dict_to_file(articles_json_data, batch_root_dir, articles_filepath)
+
+    if not os.path.exists(batch_root_dir):
+        os.makedirs(batch_root_dir)
+
+    write_dict_to_file(articles_json_data, batch_root_dir, csxjdb.constants.ARTICLES_FILENAME)
 
     raw_data_dir = os.path.join(batch_root_dir, csxjdb.constants.RAW_DATA_DIR)
     if not os.path.exists(raw_data_dir):
@@ -92,8 +93,8 @@ def save_reprocessed_batch(dest_root_dir, source_name, day_string, batch_hour_st
 
 
 def reprocess_raw_html(args):
-    provider_name, dest_root_dir = args
-    p = csxjdb.Provider(source_root, provider_name)
+    provider_name, source_root_dir, dest_root_dir = args
+    p = csxjdb.Provider(source_root_dir, provider_name)
     datasource = NAME_TO_SOURCE_MODULE_MAPPING[provider_name]
     article_count = 0
 
@@ -123,23 +124,30 @@ def reprocess_raw_html(args):
         if errors_by_batch:
             errors_by_day[day] = errors_by_batch
 
-
     err_dict = dict()
     err_dict[provider_name] = errors_by_day
     return article_count, err_dict
 
 
-def main(source_path, dest_path, processes):
-    provider_names = csxjdb.get_all_provider_names(source_root)
+def main(source_path, dest_path, processes, source_names):
+    if not os.path.exists(dest_path):
+        print "°°° Creating missing destination root:", dest_path
+        os.makedirs(dest_path)
+    provider_names = csxjdb.get_all_provider_names(source_path)
     provider_names = NAME_TO_SOURCE_MODULE_MAPPING.keys()
 
     before = datetime.now()
     n_samples = 0
     errors_by_source = dict()
 
-
-    p = mp.Pool(processes)
-    results = p.map(reprocess_raw_html, [(name, dest) for name in provider_names])
+    if processes > 1:
+        import multiprocessing as mp
+        p = mp.Pool(processes)
+        results = p.map(reprocess_raw_html, [(name, source_path, dest_path) for name in provider_names if name in source_names])
+    else:
+        results = list()
+        for name in [_ for _ in provider_names if _ in source_names]:
+            results.append(reprocess_raw_html((name, source_path, dest_path)))
 
     n_samples = sum([x[0] for x in results])
     errors_by_source = [x[1] for x in results]
@@ -147,25 +155,28 @@ def main(source_path, dest_path, processes):
     after = datetime.now()
     dt = after - before
 
-    print u"Total time for {0} articles: {1} seconds".format(n_samples, dt.seconds)
-    avg_time = float(dt.seconds) / n_samples
-    print u"Avg time per articles: {0} seconds".format(avg_time)
+    if n_samples:
+        print u"Total time for {0} articles: {1} seconds".format(n_samples, dt.seconds)
+        avg_time = float(dt.seconds) / n_samples
+        print u"Avg time per articles: {0} seconds".format(avg_time)
+    else:
+        print u"No articles were processed"
 
-    write_dict_to_file(errors_by_source, os.path.join(dest, os.path.pardir), os.path.basename(dest) + "_errors.json")
+    write_dict_to_file(errors_by_source, os.path.join(dest_path, os.path.pardir), os.path.basename(dest_path) + "_errors.json")
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Utility functions to troubleshoot queue management')
-    parser.add_argument('--source_jsondb', type=str, dest='source_jsondb', required=True, help='source json db root directory')
-    parser.add_argument('--dest_jsondb', type=str, dest='dest_jsondb', required=True, help='dest json db root directory')
+    parser.add_argument('--source-jsondb', type=str, dest='source_jsondb', required=True, help='source json db root directory')
+    parser.add_argument('--dest-jsondb', type=str, dest='dest_jsondb', required=True, help='dest json db root directory')
+    parser.add_argument('--sources', type=str, dest='source_names', default=make_parser_list(), help='comma-separated list of the sources to consider (default={0})'.format(make_parser_list()))
     parser.add_argument('--processes', type=int, dest='processes', required=False, default=1, help='Number of parallel processes to use (default=1)')
 
     args = parser.parse_args()
 
     source_root = args.source_jsondb
-    dest = args.dest_jsondb
+    dest_root = args.dest_jsondb
 
     print "Using {0} processes".format(args.processes)
-    main(source_root, dest, args.processes)
-
+    main(source_root, dest_root, args.processes, args.source_names)
