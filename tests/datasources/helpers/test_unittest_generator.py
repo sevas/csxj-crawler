e5ce9b2c2b2b22cf805e30f35b04974aebf5b1ea
sevas <f.degroef@gmail.com>
2013-03-09 22:09:16 +0100

    always indent the generate teat function, to make it easier to paste in the test class.

--- tests/datasources/helpers/test_unittest_generator.py
+++ tests/datasources/helpers/test_unittest_generator.py
@@ -14,17 +14,17 @@ def test_link_extraction_test_generator():
         csxj.datasources.helpers.unittest_generator.generate_test_func() generates a unit test function from lists of tagged urls (and other parameters)
     """
     expected_generated_code = u"""\
-def test_link_extraction_test(self):
-    with open(os.path.join(DATA_ROOT, "link_extraction_test.html")) as f:
-        article, raw_html = some_parser.extract_article_data(f)
-        extracted_links = article.links
-        group1 = [
-            make_tagged_url("http://nice.com", u\"\"\"very nice website\"\"\", set(['very nice'])),
-        ]
-        group2 = [
-        ]
-        expected_links = group1 + group2
-        assert_taggedURLs_equals(expected_links, extracted_links)
+    def test_link_extraction_test(self):
+        with open(os.path.join(DATA_ROOT, "link_extraction_test.html")) as f:
+            article, raw_html = some_parser.extract_article_data(f)
+            extracted_links = article.links
+            group1 = [
+                make_tagged_url("http://nice.com", u\"\"\"very nice website\"\"\", set(['very nice'])),
+            ]
+            group2 = [
+            ]
+            expected_links = group1 + group2
+            assert_taggedURLs_equals(expected_links, extracted_links)
 """
 
     group1 = [
